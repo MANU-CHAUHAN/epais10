52c9de8365054bca645e99331835f43fc6555d88
Manu Chauhan <manu@Manus-MacBook-Pro.local>
2020-10-01 00:26:10 +0530

    modified

--- s10.py
+++ s10.py
@@ -7,6 +7,7 @@ from dateutil.relativedelta import relativedelta
 from datetime import date
 import time
 from functools import wraps
+import random
 
 faker = Faker()
 user_count = 1000
@@ -15,7 +16,8 @@ Profile = namedtuple("Profile", faker.profile().keys())
 Profile.__doc__ = "`named tuple` for fake profile containing various fields providing information about each user's basic details"
 
 for field_ in Profile._fields:
-    getattr(Profile, field_).__doc__ = f"field `{field_}` containing information for a particular user's {field_} details"
+    getattr(Profile,
+            field_).__doc__ = f"field `{field_}` containing information for a particular user's {field_} details"
 print(help(Profile))
 
 
@@ -73,7 +75,9 @@ def calculate_metrics_namedtuples(to_print=False, *, fake_profiles: list) -> tup
 
     max_age = relativedelta(today, min(dobs))
 
-    avg_age = reduce(lambda x, y: ((x[0] + y[0]) / 2, (x[1] + y[1]) / 2, (x[2] + y[2]) / 2), [(relativedelta(today, x).years, relativedelta(today, x).months, relativedelta(today, x).days) for x in dobs])
+    avg_age = reduce(lambda x, y: ((x[0] + y[0]) / 2, (x[1] + y[1]) / 2, (x[2] + y[2]) / 2),
+                     [(relativedelta(today, x).years, relativedelta(today, x).months, relativedelta(today, x).days) for
+                      x in dobs])
     if to_print:
         print(f"Highest occurring blood group= {most_common_blood_group}")
         print(f"Mean location= {mean_location}")
@@ -98,12 +102,16 @@ def calculate_metrics_dictionary(to_print=False, *, fake_profiles: list) -> tupl
 
     most_common_blood_group = Counter([b['blood_group'] for a, b in record_dict.items()]).most_common(1)[0][0]
 
-    mean_location = reduce(lambda x, y: ((x[0] + y[0]) / 2, (x[1] + y[1]) / 2), [b['current_location'] for a, b in record_dict.items()])
+    mean_location = reduce(lambda x, y: ((x[0] + y[0]) / 2, (x[1] + y[1]) / 2),
+                           [b['current_location'] for a, b in record_dict.items()])
 
     today = date.today()
     max_age = relativedelta(today, min([b['birthdate'] for a, b in record_dict.items()]))
 
-    avg_age = reduce(lambda x, y: ((x[0] + y[0]) / 2, (x[1] + y[1]) / 2, (x[2] + y[2]) / 2), [(relativedelta(today, b['birthdate']).years, relativedelta(today, b['birthdate']).months, relativedelta(today, b['birthdate']).days) for a, b in record_dict.items()])
+    avg_age = reduce(lambda x, y: ((x[0] + y[0]) / 2, (x[1] + y[1]) / 2, (x[2] + y[2]) / 2), [(relativedelta(today, b[
+        'birthdate']).years, relativedelta(today, b['birthdate']).months, relativedelta(today, b['birthdate']).days) for
+                                                                                              a, b in
+                                                                                              record_dict.items()])
     if to_print:
         print(f"Highest occurring blood group= {most_common_blood_group}")
         print(f"Mean location= {mean_location}")
@@ -147,5 +155,45 @@ def run_task_dictionary_metrics():
     calculate_metrics_dictionary(fake_profiles=get_profiles_dictionary())
 
 
-print(run_task_namedtuple_metrics())
-print(run_task_dictionary_metrics())
+# print(run_task_namedtuple_metrics())
+# print(run_task_dictionary_metrics())
+
+def get_capitalized_letters(string):
+    """Returns joined string if characters are upper case"""
+    return ''.join(x for x in string if x.isupper())
+
+
+def stock_market():
+    """
+    To create a fake stock data set for imaginary stock exchange for top 100 companies (name, symbol, open, high, close).
+    Tasks_ToDo: Assign a random weight to all the companies. Calculate and show what value stock market started at, what was the highest value during the day and where did it end.
+    """
+    all_companies = []
+    Stocks = namedtuple("Stocks", 'name symbol open high close company_weight')
+    for _ in range(100):
+        name = faker.company()
+        open_ = round(random.uniform(41, 3999), 2)
+        high_num = round(random.uniform(0.613, 1.4), 2)  # market is damn volatile
+        high = open_ * high_num if high_num > 1.0 else open_
+        close = random.uniform(open_ - random.randint(-10, 10), high + random.randint(-8, 10))
+        if close > high:
+            high = close
+
+        all_companies.append(
+            Stocks(name=name, symbol=get_capitalized_letters(name), open=open_, high=round(high, 2),
+                   close=round(close, 2),
+                   company_weight=round(random.uniform(15, 80), 3)))
+
+    stock_index = round(sum(x.open * x.company_weight for x in all_companies), 4)
+    highest_for_day = round(max(x.high * x.company_weight for x in all_companies), 2)
+    lowest_close_for_day = round(min(x.close * x.company_weight for x in all_companies), 2)
+
+    print(f"\n------------------------------------Top 100 listed companies on TSAI Stock Exchange------------------------------------")
+    [print(x) for x in sorted(all_companies, key=lambda x:x.symbol)]
+    print(f"\n--------------Main details on {date.today()}--------------")
+    print(f"\nStock Index: {stock_index}")
+    print(f"Highest for the day: {highest_for_day}")
+    print(f"Lowest close for the day: {lowest_close_for_day}")
+
+
+stock_market()
